a58ae49e70f1be7353368ad8416b184031e711c5
Azamat Kurbanov <azik1kurbanov@gmail.com>
2021-01-07 13:05:53 +0100

    commit

---
+++ lib/macros/my_if.ex
@@ -0,0 +1,16 @@
+defmodule MyIf do
+  defmacro if(condition, clauses) do
+    do_clause = Keyword.get(clauses, :do, nil)
+    else_clause = Keyword.get(clauses, :else, nil)
+    quote do
+      case unquote(condition) do
+        val when val in [false, nil] -> unquote(else_clause)
+        _ -> unquote(do_clause)
+      end
+    end
+  end
+end
+
+defmodule TestMyIf do
+  require MyIf
+end
--- lib/macros/times.ex
+++ lib/macros/times.ex
@@ -1,16 +1,16 @@
 defmodule Times do
   defmacro times_n(n) do
     quote do
-      def unquote(:"times_#{n}")(m), do: m * unquote(n)
+      def unquote(:"times_#{n}")(factor), do: factor * unquote(n)
     end
   end
 end
 
-defmodule TestTimes do
+defmodule TestMyMac do
   require Times
   Times.times_n(3)
   Times.times_n(4)
 end
 
-IO.puts TestTimes.times_3(4)
-IO.puts TestTimes.times_4(5)
+IO.puts TestMyMac.times_3(4)
+IO.puts TestMyMac.times_4(5)
